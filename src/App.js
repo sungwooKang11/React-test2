9126c93dea080870bbdd372577970808c4df0444
sungwookang <sungwookang11@gmail.com>
2022-03-12 00:44:50 +0900

    ..

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import './App.css';
 
 function App() {
   
+  let posts = "HelloWorld";
+  function good(){
+    return 100
+  }
+
   return (
     <div className="App">
       <div className="nav-bar">
-        <div className="home"></div>
+        <h2> {posts} </h2>
+        
       </div>
     </div>
   );
